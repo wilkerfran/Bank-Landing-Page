3419c3df40be5d7784e5cd06c878ecc7567c5b3d
wilkerfran <wilkerfran@gmail.com>
2024-11-13 23:18:40 -0300

    Header Complete

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,58 @@
 import Image from "next/image";
 
 import Logo from '@/assets/logo.svg';
-import Arrow from '@/assets/arrow-down.svg';
+import IconUser from '@/assets/icon-user.svg';
+import { ItemMenu } from "./ItemMenu";
+import { Search } from "./Search";
 
 
 export function Header() {
   return (
-    <header className="w-full h-20 bg-primary-orange">
-        <div className="w-full max-w-[1246px] px-[15px] mx-auto">
-            <div className="flex items-start gap-14">
-                <div>
+    <header className="relative flex items-center justify-between w-full h-20 bg-primary-orange">
+        <div className="absolute top-0 right-0 bg-primary-blue w-[19%] h-full z-0"></div>
+        <div className="flex items-center w-full max-w-[1246px] px-[15px] mx-auto">
+            <div className="flex flex-1 items-center justify-between">
+                <div className="flex items-center gap-14">
                     <Image 
                         src={Logo}
                         alt="Logo"
                     />
-                    <ul>
+                    <ul className="flex  items-center gap-12">
                         <li>
-                            <button>
-                                <span>Para você</span>
-                                <Image 
-                                    src={Arrow}
-                                    alt="Arrow dropdown" 
-                                />
-                            </button>
+                            <ItemMenu 
+                                name="Para você"
+                            />
+                            
+                        </li>
+                        <li>
+                            <ItemMenu 
+                                name="Para empresas"
+                            />
+                            
+                        </li>
+                        <li>
+                            <ItemMenu 
+                                name="Serviços"
+                            />
+                            
+                        </li>
+                        <li>
+                            <ItemMenu 
+                                name="Ajuda"
+                            />
+                            
                         </li>
                     </ul>
                 </div>
+                <Search />
             </div>
+            <button className="flex items-center gap-4 bg-primary-blue h-20 pl-10 z-10">
+                <Image 
+                    src={IconUser}
+                    alt="User"  
+                />
+                <span className="text-white font-bold">Acessar conta</span>
+            </button>
         </div>
     </header>
   );
